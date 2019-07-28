c7f9eab6298abf556b91a6c2a52b71fc1dd2ff38
phamvnquy <phamvanquy0910@gmail.com>
2019-07-28 15:11:03 +0700

    nopbai

---
+++ session23/js.js
@@ -0,0 +1,10 @@
+function bt() {
+	var c = 12;
+	var v = 4;
+for (var i = 0; i < v; i++) {
+	var tong= c*2;
+	c=tong;
+}
+document.write('so qua cam ban dau cos: '+c);
+}
+bt();
---
+++ session23/ss23.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ss23</title>
+</head>
+<body>
+	<script type="text/javascript" src="js.js"></script>
+</body>
+</html>
